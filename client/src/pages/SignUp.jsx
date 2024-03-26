b010768a137fc0f792b2495b3129998abeed021b
jatingarg0410 <gargjatin0410@gmail.com>
2024-03-26 21:09:22 +0530

    changes in signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
-const SignUp = () => {
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function SignUp() {
   const [formData, setFormData] = useState({})
   const [errorMessage, setErrorMessage] = useState(null)
   const [loading, setLoading] = useState(false)
@@ -39,17 +40,19 @@ const SignUp = () => {
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
-        <div className="">
-          <Link to="/" className=" font-bold dark:text-white text-4xl">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Jatin's Blog
+              Jatin's
             </span>
+            Blog
           </Link>
           <p className="text-sm mt-5">
-            You can SignUp with your email and password or with Google
+            You can sign up with your email and password or with Google.
           </p>
         </div>
         {/* right */}
+
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
@@ -110,5 +113,3 @@ const SignUp = () => {
     </div>
   )
 }
-
-export default SignUp
