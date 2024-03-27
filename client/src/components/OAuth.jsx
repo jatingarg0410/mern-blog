eac7a91beff3bd997628aa720c8ea6c5612b2afe
jatingarg0410 <gargjatin0410@gmail.com>
2024-03-27 13:25:46 +0530

    make the dashboard private

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,12 +1,12 @@
 import { Button } from 'flowbite-react'
 import { AiFillGoogleCircle } from 'react-icons/ai'
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { app } from '../firebase'
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice'
 import { useNavigate } from 'react-router-dom'
-import { app } from '../firebase'
 
-const OAuth = () => {
+export default function OAuth() {
   const auth = getAuth(app)
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -17,9 +17,7 @@ const OAuth = () => {
       const resultsFromGoogle = await signInWithPopup(auth, provider)
       const res = await fetch('/api/auth/google', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           name: resultsFromGoogle.user.displayName,
           email: resultsFromGoogle.user.email,
@@ -47,5 +45,3 @@ const OAuth = () => {
     </Button>
   )
 }
-
-export default OAuth
