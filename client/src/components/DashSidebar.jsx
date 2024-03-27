6fc1c792d1fe390a401caba3eecaeedfde6b2472
jatingarg0410 <gargjatin0410@gmail.com>
2024-03-27 16:23:47 +0530

    complete image upload

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -29,7 +29,6 @@ const DashSidebar = () => {
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Link to="/dashboard?tab=profile">
-            {' '}
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
